1d3cd9b23e408ec64564248854e77dc736abd7be
Martynkin Slavamir <sss_mmm86@mail.ru>
2024-02-14 23:26:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                if (int.TryParse(args[0], out int num1) && int.TryParse(args[2], out int num2))
+                {
+                    if (args[1] == "+")
+                    {
+                        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                    }
+                    if (args[1] == "-")
+                    {
+                        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    }
+                    if (args[1] == "*")
+                    {
+                        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    }
+                    if (args[1] == "/")
+                    {
+                        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                    }
+                    else Console.WriteLine("Наш калькулятор не знает такого действия");
+                }
+            }
+            else Console.WriteLine("Корректно введите пример!");
+        }
+    }
+    }
+}
